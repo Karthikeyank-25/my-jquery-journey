1426bbfafaf16a8b163e0e82b3865e18af2789cf
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 13:34:08 +0530

    Selecting Elements

---
+++ selecting elements.js
@@ -0,0 +1,12 @@
+// Selecting Elements :
+//              Selecting the elements in the document by using jQuery library.
+
+// Syntax :         (Same for selecting single and multiple elements)
+
+// Selecting one element :
+//              Instead of using "document.querSelector('h1')" to select one element.
+document.querySelector = $("h1");
+
+// Selecting all element :
+//              Instead of using "document.querSelectorAll('h1')" to select one element.
+document.querySelectorAll = $("h1");
