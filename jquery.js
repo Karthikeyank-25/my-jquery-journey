1bdfb821e633252cd9f7591f490d965f20730609
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 10:23:30 +0530

    Jquery

---
+++ jquery.js
@@ -0,0 +1,14 @@
+// What is jQuery?
+//          - jQuery is a fast, small, and feature-rich JavaScript library.
+//          - It makes HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers.
+//          - Released in 2006 by John Resig.
+//          - Very popular for simplifying JavaScript programming, especially before modern frameworks took off.
+
+// Syntax :
+
+            $(selector).action();
+
+// $ is the jQuery function.
+// selector is used to select HTML elements.
+
+
