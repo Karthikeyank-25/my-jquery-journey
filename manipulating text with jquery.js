944f7eaefca532153de48449f5efc516a255b385
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 14:16:26 +0530

    Manipulating text with jQuery

---
+++ manipulating text with jquery.js
@@ -0,0 +1,7 @@
+// Manipulating text with jQuery :
+//              Manipulating text in the javaScript using jQuery library.
+
+// Manipulating text :
+
+$("h1").text("Hi I'm Karthikeyan!");        // change and shows text whatever inbetween of ("")
+$("h1").html("<h5>I'm big</h5>");           // change text asusual in html by adding tag and text in between of them and tags are not shown
