0c493418c846ede350a451224d869682852a874a
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 11:31:34 +0530

    Incorporate jQuery

---
+++ incorporate jQuery.js
@@ -0,0 +1,8 @@
+// Incorporate jQuery :
+//              Adding jQuery into your web project so you can use its features.
+//              You add the jQuery library by including a <script> tag in your HTML, usually inside the <head> or just before the closing </body> tag.
+
+//  CDN (Content Delivery Network) :
+//               Just add this inside your HTML file.
+//               Above the script tag that links to the javascript file into the html file.
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
