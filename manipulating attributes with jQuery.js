9c7c6a2fdda6143440a014f1c5aac7edc0a82f8e
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 14:27:46 +0530

    Manipulating attributes with jQuery

---
+++ manipulating attributes with jQuery.js
@@ -0,0 +1,7 @@
+// Manipulating attributes with jQuery :
+//              Manipulating the attributes of the documents by adding jQuery methods in javaScript.
+
+// Manipulating Attributes :
+
+$("h1").attr("class");                          // getting attributes
+$("h1").attr("src","https://www.google.com");   // setting attributes
