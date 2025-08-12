a03c6e2635aab6d6c0d4223194395e7b4a78a737
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-12 13:18:47 +0530

    Minification

---
+++ minification.js
@@ -0,0 +1,5 @@
+// Minification :
+//          - Minification is the process of removing all unnecessary characters from source code without changing its functionality. 
+//          - This typically applies to web development files like JavaScript, CSS, and HTML to reduce file size and improve load times.
+
+"https://www.minifier.org/"     // Where to minificate our codes online 
